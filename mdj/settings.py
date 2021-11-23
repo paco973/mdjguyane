6bdd0bf5c8e622ed0b27d968e09806b1fcbf278e
paco973 <70005668+paco973@users.noreply.github.com>
2021-11-23 10:46:36 +0100

    test
    test

--- mdj/settings.py
+++ mdj/settings.py
@@ -90,55 +90,36 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-
 DATABASES = {
-
-
-
-
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mdjguyane',
-        'USER': 'paco@mdjguyane',
-        'PASSWORD': 'PAPApadeo1',
-        'HOST': 'mdjguyane.postgres.database.azure.com',
-        'PORT': '5432',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mdjguyane',
+            'USER': 'paco@mdjguyane',
+            'PASSWORD': 'PAPApadeo1',
+            'HOST': 'mdjguyane.postgres.database.azure.com',
+            'PORT': '5432',
+        }
     }
-}
 
 
 '''
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mdjguyane',
-        'USER': 'paco',
-        'PASSWORD': 'PAPApadeo1',
-        'HOST': '/cloudsal/mdjguyane:us-central1:myinstance',
-    }
-}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES = {
-
-
-
-
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mdjguyane',
-        'USER': 'paco',
-        'PASSWORD': 'PAPApadeo1',
-        'HOST': 'mdjguyane.postgres.database.azure.com',
-        'PORT': '5432',
+ DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mdjguyane',
+            'USER': 'paco@mdjguyane',
+            'PASSWORD': 'PAPApadeo1',
+            'HOST': 'mdjguyane.postgres.database.azure.com',
+            'PORT': '5432',
+        }
     }
-}
 
 '''
 # Password validation
